f1bdc712b399836eb5a946c90f3bb4d8cc958c1e
echo complex <lexa33232@gmail.com>
2024-08-21 21:03:32 +0300

    Update

--- script/index.js
+++ script/index.js
@@ -10,14 +10,45 @@ if (ScrollTrigger.isTouch !== 1) {
 }
 
 // ScrollTrigger.create({
-//     trigger: "welcome",
-//     start: "top center", // Когда верх элемента достигает центра экрана
+//     trigger: '#what-is-prp',
+//     markers: true,
+//     start: "top 50%",
+//     end: "bottom 0%", 
+  
 //     onEnter: () => {
-//         document.documentElement.style.setProperty('--bg-color', '#FFF'); // Изменяем значение CSS переменной
-//         document.documentElement.style.setProperty('--text-color', '#77A6F7');
+//       gsap.to('#what-is-prp', { duration: 0.5, opacity: 1 })
 //     },
+    
 //     onLeaveBack: () => {
-//         document.documentElement.style.setProperty('--text-color', '#FFF');
-//         document.documentElement.style.setProperty('--bg-color', '#77A6F7'); // Возвращаем значение при прокрутке вверх
-//     }
-// });
+//       gsap.to('#what-is-prp', { duration: 0.5, opacity: 0 })
+//     }, 
+// })
+
+ScrollTrigger.create({
+    trigger: '#prp-therapy',
+    markers: true,
+    start:"top 50%",
+    end:"bottom 0%", 
+  
+    onEnter: () => {
+        gsap.to('#what-is-prp', { duration: 1, backgroundColor: '#FFF' })
+        gsap.to('#what-is-prp img ', { duration: 1, opacity: 0 })
+        gsap.to('#what-is-prp b', { duration: 1, opacity: 0 })
+        gsap.to('#prp-therapy', { duration: 1, backgroundColor: '#FFF'})
+        gsap.to('#prp-therapy h1', { duration: 1, color: '#77A6F7'})
+        gsap.to('#prp-therapyinfo', { duration: 1, opacity: 1 })
+        gsap.to('#prp-therapyinfo p', { duration: 1, color: '#77A6F7'})
+    },
+    
+    onLeaveBack: () => {
+        gsap.to('#what-is-prp', { duration: 1, backgroundColor: '#77A6F7'})
+        gsap.to('#what-is-prp img', { duration: 1, opacity: 1 })
+        gsap.to('#what-is-prp b', { duration: 1, opacity: 1 })
+        gsap.to('#prp-therapyinfo', { duration: 1, opacity: 0 })
+        gsap.to('#prp-therapy', { duration: 1, backgroundColor: '#77A6F7'})
+        gsap.to('#prp-therapy h1', { duration: 1, color: '#77A6F7'})
+        gsap.to('#prp-therapyinfo p', { duration: 1, color: '#FFF'})
+    },
+  
+    
+})
