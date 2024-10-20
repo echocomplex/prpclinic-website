d2a87664aee44dc22e32732d966c9047e0f1a9e1
echo complex <lexa33232@gmail.com>
2024-10-20 16:06:38 +0300

    Update

--- page/script/index.js
+++ page/script/index.js
@@ -4,12 +4,12 @@ if (ScrollTrigger.isTouch !== 1) {
     ScrollSmoother.create({
         wrapper: ".wrapper",
         content: ".content",
-        smooth: 1, 
+        smooth: 0.9, 
         effects: true
     });
 }
 else {
-    window.location.replace("https://mobile.ru");
+    window.location.replace("mobile");
 }
 
 ScrollTrigger.create({
@@ -29,7 +29,7 @@ ScrollTrigger.create({
         gsap.to('#what-is-prp', { duration: 1, opacity: 1 })
         gsap.to('#prp-therapy', { duration: 1, opacity: 0 })
     },
-})
+});
 
 ScrollTrigger.create({
     trigger: '#procedure',
@@ -48,7 +48,7 @@ ScrollTrigger.create({
         gsap.to('#procedure', { duration: 1, opacity: 0 })
         gsap.to('#prp-therapy', { duration: 1, opacity: 1 })
     },
-})
+});
 
 ScrollTrigger.create({
     trigger: '#reasons',
@@ -66,5 +66,79 @@ ScrollTrigger.create({
         gsap.to('body', { duration: 1, backgroundColor: '#77A6F7' })
         gsap.to('#reasons', { duration: 1, opacity: 0 })
         gsap.to('#procedure', { duration: 1, opacity: 1 })
+    },
+});
+
+ScrollTrigger.create({
+    trigger: '#list-of-serivces',
+    marker:true,
+    start: "top 50%",
+    end: "bottom 0%", 
+  
+    onEnter: () => {
+        gsap.to('body', { duration: 1, backgroundColor: '#77A6F7' })
+        gsap.to('#list-of-serivces', { duration: 1, opacity: 1 })
+        gsap.to('#reasons', { duration: 1, opacity: 0 })
+    },
+    
+    onLeaveBack: () => {
+        gsap.to('body', { duration: 1, backgroundColor: '#FFF' })
+        gsap.to('#list-of-serivces', { duration: 1, opacity: 0 })
+        gsap.to('#reasons', { duration: 1, opacity: 1 })
+    },
+});
+
+document.getElementById('gotoPRP').addEventListener('click', function() {
+    window.open('about-prp');
+});
+
+document.getElementById('gotoNeurology').addEventListener('click', function() {
+    window.open('about-neurology');
+});
+
+document.getElementById('gotoMassage').addEventListener('click', function() {
+    window.open('about-massage');
+});
+
+document.getElementById('gotoMammology').addEventListener('click', function() {
+    window.open('about-mammology');
+});
+
+document.getElementById('gotoOncology').addEventListener('click', function() {
+    window.open('about-oncology');
+});
+
+document.getElementById('gotoTests').addEventListener('click', function() {
+    window.open('about-tests');
+});
+
+document.getElementById('gotoOrthopedics').addEventListener('click', function() {
+    window.open('about-orthopedics');
+});
+
+document.getElementById('gotoManualTherapy').addEventListener('click', function() {
+    window.open('about-manual-therapy');
+});
+
+document.getElementById('gotoPhysiotherapy').addEventListener('click', function() {
+    window.open('about-physiotherapy');
+});
+
+ScrollTrigger.create({
+    trigger: '#contacts-and-appointment',
+    marker:true,
+    start: "top 50%",
+    end: "bottom 0%", 
+  
+    onEnter: () => {
+        gsap.to('body', { duration: 1, backgroundColor: '#FFF' })
+        gsap.to('#contacts-and-appointment', { duration: 1, opacity: 1 })
+        gsap.to('#list-of-serivces', { duration: 1, opacity: 0 })
+    },
+    
+    onLeaveBack: () => {
+        gsap.to('body', { duration: 1, backgroundColor: '#77A6F7' })
+        gsap.to('#contacts-and-appointment', { duration: 1, opacity: 0 })
+        gsap.to('#list-of-serivces', { duration: 1, opacity: 1 })
     },
 })
