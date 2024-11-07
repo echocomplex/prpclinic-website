e314ae513d2c30c9071644d30a66af117425caad
Alice White <alicewhite@AliceWinPC.>
2024-11-07 21:39:46 +0300

    Update

--- page/appointment/script/index.js
+++ page/appointment/script/index.js
@@ -5,3 +5,13 @@ document.getElementById('logo').addEventListener('click', function() {
 document.getElementById('note').addEventListener('click', function() {
         window.location.href = '../index.html';
 });
+
+$(document).ready(function() {
+        $('#phone').inputmask({
+            mask: '+7 (999) 999-99-99',
+            placeholder: '+7 (___) ___-__-__',
+            showMaskOnHover: true,
+            showMaskOnFocus: true
+        });
+});
+  
--- page/script/index.js
+++ page/script/index.js
@@ -4,7 +4,7 @@ if (ScrollTrigger.isTouch !== 1) {
     ScrollSmoother.create({
         wrapper: ".wrapper",
         content: ".content",
-        smooth: 0.9, 
+        smooth: 1, 
         effects: true
     });
 }
@@ -162,7 +162,7 @@ ScrollTrigger.create({
 });
 
 document.getElementById('gotoAppointment').addEventListener('click', function() {
-    window.open('appointment');
+    window.open('appointment/index.html');
 });
 
 document.getElementById('gotoVK').addEventListener('click', function() {
