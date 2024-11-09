b24dbb142044df5c0ee120c32a36ebeeaf529d33
Alice White <alicewhite@AliceWinPC.>
2024-11-09 15:33:49 +0300

    Update

--- page/appointment/script/index.js
+++ page/appointment/script/index.js
@@ -13,5 +13,31 @@ $(document).ready(function() {
             showMaskOnHover: true,
             showMaskOnFocus: true
         });
+        $('#phone').on('input', function() {
+                var isComplete = $(this).inputmask('isComplete');
+                var name = $('#name').val().trim();
+                if (isComplete && name.length > 0) {
+                        $('#startAppointment').prop('disabled', false);
+                } 
+                else {
+                        $('#startAppointment').prop('disabled', true);
+                }
+        });
+        $('#name').on('input', function() {
+                var name = $(this).val().trim();
+                var isComplete = $('#phone').inputmask('isComplete');
+        
+                if (isComplete && name.length > 0) {
+                        $('#startAppointment').prop('disabled', false);
+                } 
+                else {
+                        $('#startAppointment').prop('disabled', true);
+                }
+        });
+        $('#startAppointment').prop('disabled', true);
 });
+
+function generateLink() {
+        window.location.href = '../index.html';
+}
   
